6fd8f434557ea5138032e6589d1d8f6be9a82156
Daniela Correa <correadaniela783@gmail.com>
2023-03-30 18:14:30 -0500

    changes-generales

--- src/views/Pokedex.jsx
+++ src/views/Pokedex.jsx
@@ -32,46 +32,46 @@ const Pokedex = () => {
   return (
     <div className="w-full p-5">
       <div className="container img">
-      <p className="pokedex-container">
-        <span className="text-res-100 font-semibold">Bienvenido {user}, </span>
-        Aqui podras encontrar tu pokemon favorito
-      </p>
+        <p className="pokedex-container">
+          <span className="text-res-100 font-semibold">Bienvenido {user}, </span>
+          Aqui podras encontrar tu pokemon favorito
+        </p>
 
-      <div className="paginacion">
-        {pokemonsPagination.pages.map((page) => (
-          <button
-            key={page}
-            onClick={() => pokemonsPagination.changePageTo(page)}
-            className={pokemonsPagination.currentPage === page ? 'text-yellow-500' : ''}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
+        <div className="paginacion">
+          {pokemonsPagination.pages.map((page) => (
+            <button
+              key={page}
+              onClick={() => pokemonsPagination.changePageTo(page)}
+              className={pokemonsPagination.currentPage === page ? 'text-yellow-500' : ''}
+            >
+              {page}
+            </button>
+          ))}
+        </div>
 
-      <div className="container-select">
-        <Form className="search">
-          <h3 className="text-red-500">Filter for search</h3>
-          <input
-            type="text"
-            name="pokemon_name"
-            className="input"
-            value={pokemonName}
-            onChange={handleNameChange}
-          />
-          <div className="selectAll">
-            <select name="pokemon_type" value={pokemonType} onChange={handleTypeChange}>
-              <option value="">All</option>
-              {types.map((type) => (
-                <option key={type.url} value={type.name}>
-                  {type.name}
-                </option>
-              ))}
-            </select>
-            <button className="button-container second">Search</button>
-          </div>
-        </Form>
-      </div>
+        <div className="container-select">
+          <Form className="search">
+            <h3 className="text-red-500">Filter for search</h3>
+            <input
+              type="text"
+              name="pokemon_name"
+              className="input"
+              value={pokemonName}
+              onChange={handleNameChange}
+            />
+            <div className="selectAll">
+              <select name="pokemon_type" value={pokemonType} onChange={handleTypeChange}>
+                <option value="">All</option>
+                {types.map((type) => (
+                  <option key={type.url} value={type.name}>
+                    {type.name}
+                  </option>
+                ))}
+              </select>
+              <button className="button-container second">Search</button>
+            </div>
+          </Form>
+        </div>
       </div>
 
       <section>
